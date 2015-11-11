87155770f6569a01a43ff6b1bc90bcfd9360fd1b
Ahan Sabharwal <me@ahansabharwal.com>
2015-11-11 20:48:51 +0530

    Added live counter, fixed major bugs

--- script.js
+++ script.js
@@ -5,19 +5,20 @@ var r = 0.011;
 var t = 1/(365*24*3600);
 var c1 = 432;
 var c2 = 432/(24*3600);
-/*
-t = (Math.log(nFuture/nCurrent))/r
-nCurrent = 7378625150 * Math.pow(Math.E, (-r*t));
-window.alert(nCurrent);
-var create = document.getElementById("pop"),innerHTML;
-var res = create.replace("number", nCurrent);
-document.getElementById("pop").innerHTML = res;*/
+
+function ReplaceContent(id,content) {
+var container = document.getElementById(id);
+container.innerHTML = content;
+}
+
 time=setInterval(function(){
 
 nFuture = Math.round(nCurrent * Math.pow(Math.E, (r*t)));
-
-var co2 = nFuture * c2;
+var difference = nFuture - nCurrent;
+nCurrent = nCurrent + difference;
+var co2 = nCurrent * c2;
 //window.alert(co2 + "L");
-window.alert(co2 + "L");
+//window.alert(co2 + "L");
+ReplaceContent("pop", co2 + " L");
 },1000);
 //7378847523, 7378625153 7378625150
